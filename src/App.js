fbadb9054156a5b96f05ac4abcfc6c3ee3daa93a
unknown <boukhris.hechem@gmail.com>
2020-07-16 10:43:56 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <div> 
+    <div style={{border:'solid 1px black',maxWidth:'100vw'}}> 
+ 
+    <h1 className='title red'>Your name here </h1> 
+
+          <img src="/logo192.png" />  
+
+         <img src={logo}/> 
+</div>  
+
+  {/* <video style ={{width='320', height='240'}} controls> 
+
+      <source src='myVideo.mp4' type='video/mp4'/>
+
+  </video>  */}
+ </div>)
+}
+
+export default App;
